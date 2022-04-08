514e18897293decda1d812ff74d72452370e2799
AndyMcMagadan <mcmagadan@mail.ru>
2022-04-08 13:02:35 +0300

    lesson_2_1. Send email to file.

--- authnapp/urls.py
+++ authnapp/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import re_path
 
 import authnapp.views as authnapp
@@ -11,4 +10,6 @@ urlpatterns = [
     re_path(r"^logout/$", authnapp.logout, name="logout"),
     re_path(r"^register/$", authnapp.register, name="register"),
     re_path(r"^edit/$", authnapp.edit, name="edit"),
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$",
+            authnapp.verify, name="verify"),
 ]
